f89eafdaed41d3e33a4b848c21f3dae611803abf
Jett Pennington <jettpennington@gmail.com>
2023-10-03 21:36:19 +0000

    added some TT functionalaty

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ let renderer = {
     },
     makeContainer: function (id, where, containerClass = "container", rowClass = "row", colClass = "col") {
         this.makeTag("div", id + "Cont", where, containerClass)
-        this.makeTag("div", id + "Row", id + "Cont", rowClass)
-        this.makeTag("div", id + "Col", id + "Row", colClass)
+        this.makeTag("div", id + "Row0", id + "Cont", rowClass)
+        this.makeTag("div", id + "Col0", id + "Row0", colClass)
     },
     setAtrubute: function (tagId, atrubute, value) {
         let tag = document.getElementById(tagId);
@@ -38,5 +38,97 @@ let renderer = {
         renderer.makeTag("img", id, where, classAttribute)
         renderer.setAtrubute(id, "src", imgLink)
         renderer.setAtrubute(id, "alt", altText)
+    },
+    makeTTTRow: function (id, rowNum, colIds, rowClass = "row", colClass = "col"){
+        this.makeTag("div", id + "Row" + rowNum, id + "Cont", rowClass)
+        this.makeTag("div", id + "Col" + colIds[0], id + "Row" + rowNum, colClass)
+        this.makeTag("div", id + "Col" + colIds[1], id + "Row" + rowNum, colClass)
+        this.makeTag("div", id + "Col" + colIds[2], id + "Row" + rowNum, colClass)
+    },
+    drawTTTBoard: function (id, where, containerClass = "container", rowClass = "row", colClass = "col") {
+        this.makeContainer(id, where, containerClass, rowClass, colClass)
+        this.makeTag("div", id + "Col1", id + "Row0", colClass)
+        this.makeTag("div", id + "Col2", id + "Row0", colClass)
+        this.makeTTTRow(id, 1, [3,4,5], rowClass, colClass)
+        this.makeTTTRow(id, 2, [6,7,8], rowClass, colClass)
+    },
+    clickhandelTTTBoard: function (){
+        
     }
 };
+
+let pageController = {
+    
+    currentGame: "",
+
+    playTicTacToe: function(){
+        this.currentGame = "TTT"
+    },
+    
+    playConect4: function() {
+        this.currentGame = "C4"
+    }
+};
+
+let TTTController = {
+
+    boardState: [
+                ["x","","o"],
+                ["x","o",""], 
+                ["o","","x"]
+                ],
+
+    turnX: true,
+
+    populateBoard: function(board, boardId){
+        let count = 0;
+        for (let subarr of board){
+            for (let tile of subarr){
+                renderer.drawText(tile, boardId + "Col" + count)
+                count++;
+            }
+        }
+    },
+
+    boxClicked: function(indexArr, XorO) {
+        this.boardState[indexArr[0]][indexArr[1]] = XorO;
+        renderer.clearPage("div1")
+        this.drawBoard()
+    },
+
+    convertStrArrToBoolArr: function(twoDArr, XorO){
+        let out = []
+        for (let subarr of twoDArr){
+            let innerArr = [];
+            for (let tile of subarr){
+                if (tile === XorO){
+                    innerArr.push(true)
+                }else{
+                    innerArr.push(false)
+                }
+            }
+            out.push(innerArr);
+        }
+        return out
+    },
+
+    hasPlayerWon: function(arr) {
+        let horazontal = ((arr[0][0] && arr[0][1] && arr[0][2]) || (arr[1][0] && arr[1][1] && arr[1][2]) || (arr[2][0] && arr[2][1] && arr[2][2]));
+
+        let vertical = ((arr[0][0] && arr[1][0] && arr[2][0]) || (arr[0][1] && arr[1][1] && arr[2][1]) || (arr[0][2] && arr[1][2] && arr[2][2]));
+
+        let diaganal = ((arr[0][0] && arr[1][1] && arr[2][2]) || (arr[0][2] && arr[1][1] && arr[2][0]))
+
+        return horazontal || vertical || diaganal
+    },
+
+    
+
+};
+
+renderer.drawTTTBoard("TTTBoard", "div1")
+TTTController.populateBoard(TTTController.boardState, "TTTBoard")
+
+console.log("X: " + TTTController.hasPlayerWon(TTTController.convertStrArrToBoolArr(TTTController.boardState, "x")))
+
+console.log("O: " + TTTController.hasPlayerWon(TTTController.convertStrArrToBoolArr(TTTController.boardState, "o")))
