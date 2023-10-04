4f7d53d175f4eb63125e1260bf9d23de9c83978f
Jett Pennington <jettpennington@gmail.com>
2023-10-04 17:30:27 +0000

    updated TTT functionalaty

--- main.js
+++ main.js
@@ -39,6 +39,15 @@ let renderer = {
         renderer.setAtrubute(id, "src", imgLink)
         renderer.setAtrubute(id, "alt", altText)
     },
+    drawContentBox: function (id, where, headText, numberofSubBoxes, SubBoxTextArray) {
+        this.makeContainer(id, where, "container", "row ", "col")
+        this.drawText(headText, id + "Col")
+        this.makeTag("div", id + "Row2", id + "Cont", "row")
+        for (let i = 0; i < numberofSubBoxes; i++) {
+            this.makeTag("div", id + "Col" + i, id + "Row2", "col-" + (12 / numberofSubBoxes))
+            this.drawText(SubBoxTextArray[i], id + "Col" + i)
+        }
+    },
     makeTTTRow: function (id, rowNum, colIds, rowClass = "row", colClass = "col"){
         this.makeTag("div", id + "Row" + rowNum, id + "Cont", rowClass)
         this.makeTag("div", id + "Col" + colIds[0], id + "Row" + rowNum, colClass)
@@ -52,8 +61,19 @@ let renderer = {
         this.makeTTTRow(id, 1, [3,4,5], rowClass, colClass)
         this.makeTTTRow(id, 2, [6,7,8], rowClass, colClass)
     },
-    clickhandelTTTBoard: function (){
-        
+    clickhandelTTTBoard: function (boardId, turn){
+        for (i = 0; i < 9; i++){
+            this.makeEvent(boardId + "Col" + i, "click", "TTTController.boxClicked", "(["+ Math.floor(i/3) + ", " + i % 3 + "], " + "'" + turn + "'" + ")")
+        }
+    },
+    drawTTTHeadder: function(id, where, headTextarr, SubBoxTextArray, containerClass = "container", rowClass = "row", colClass = "col"){
+        this.makeContainer(id, where, containerClass, rowClass, colClass)
+        this.makeTag("div", id + "Col1", id + "Row0", colClass)
+        this.makeTag("div", id + "Col2", id + "Row0", colClass)
+
+        this.drawContentBox(id + "Box0", id + "Col0", headTextarr[0], )
+        this.drawContentBox()
+
     }
 };
 
@@ -73,27 +93,33 @@ let pageController = {
 let TTTController = {
 
     boardState: [
-                ["x","","o"],
-                ["x","o",""], 
-                ["o","","x"]
+                ["","",""],
+                ["","",""], 
+                ["","",""]
                 ],
 
-    turnX: true,
+    turn: "o",
+
+    turnCount: 0,
 
     populateBoard: function(board, boardId){
         let count = 0;
         for (let subarr of board){
             for (let tile of subarr){
-                renderer.drawText(tile, boardId + "Col" + count)
+                renderer.drawText(tile.toUpperCase(), boardId + "Col" + count)
                 count++;
             }
         }
+    },
+    populateHeadder: function(){
+
     },
 
     boxClicked: function(indexArr, XorO) {
         this.boardState[indexArr[0]][indexArr[1]] = XorO;
         renderer.clearPage("div1")
-        this.drawBoard()
+        this.updateBoard()
+
     },
 
     convertStrArrToBoolArr: function(twoDArr, XorO){
@@ -121,13 +147,33 @@ let TTTController = {
 
         return horazontal || vertical || diaganal
     },
+    
+    updateBoard: function(){
+        if (TTTController.hasPlayerWon(TTTController.convertStrArrToBoolArr(TTTController.boardState, this.turn)) || this.turnCount > 8){
+            renderer.clearPage("div1");
+            renderer.drawTTTBoard("TTTBoard", "div1", "container", "row", "col TTT text-center rounded");
+            TTTController.populateBoard(TTTController.boardState, "TTTBoard");
+        }else {
+            if (this.turn === "x"){
+                this.turn = "o"
+            }
+            else if (this.turn === "o"){
+                this.turn = "x"
+            }
+            this.turnCount++;
+
+            renderer.clearPage("div1");
+            renderer.drawTTTBoard("TTTBoard", "div1", "container", "row", "col TTT text-center rounded");
+            TTTController.populateBoard(TTTController.boardState, "TTTBoard");
+            renderer.clickhandelTTTBoard("TTTBoard", this.turn);
+        }
+    }
 
     
 
 };
 
-renderer.drawTTTBoard("TTTBoard", "div1")
-TTTController.populateBoard(TTTController.boardState, "TTTBoard")
+TTTController.updateBoard()
 
 console.log("X: " + TTTController.hasPlayerWon(TTTController.convertStrArrToBoolArr(TTTController.boardState, "x")))
 
